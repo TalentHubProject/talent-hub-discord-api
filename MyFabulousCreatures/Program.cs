2131d3fd73a97b164e4f7f27602a461c74d5ec5e
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-01-05 08:53:58 +0100

    fix(file-service): register good recently refactored service

--- MyFabulousCreatures/Program.cs
+++ MyFabulousCreatures/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IFileImageService, FileImageService>();
+builder.Services.AddScoped<IStaticFileService, StaticFileService>();
 
 var app = builder.Build();
 
