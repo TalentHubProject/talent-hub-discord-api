a36bb48d97945142e4a25ef213c8e7766a63cc94
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-01-10 14:39:07 +0100

    feat(model): add dto attributes annotations

--- MyFabulousCreatures/Models/Creature.cs
+++ MyFabulousCreatures/Models/Creature.cs
@@ -2,6 +2,8 @@
 // Licensed under the GNU General Public License v3.0.
 // See the LICENSE file in the project root for more information.
 
+using System.ComponentModel.DataAnnotations;
+
 namespace MyFabulousCreatures.Models;
 
-public record Creature(string Type, int Age = 0);
+public record Creature(string Type, [Range(1, 3)] int Age = 0);
--- MyFabulousCreatures/Models/Egg.cs
+++ MyFabulousCreatures/Models/Egg.cs
@@ -2,6 +2,8 @@
 // Licensed under the GNU General Public License v3.0.
 // See the LICENSE file in the project root for more information.
 
+using System.ComponentModel.DataAnnotations;
+
 namespace MyFabulousCreatures.Models;
 
-public record Egg(string CreatureType, int Cracks = 0);
+public record Egg(string CreatureType, [Range(0, 4)] int Cracks = 0);
